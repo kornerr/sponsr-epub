7001fae930fcd95a63615e34824d2c24da18529c
Михаил Капелько <kornerr@gmail.com>
2025-07-05 23:53:25 +0300

    add a few templates

--- constants.py
+++ constants.py
@@ -9,4 +9,32 @@ FIRST_POST = "https://sponsr.ru/marahovsky/?post_date=1585688400000"
 SELENIUM_DRIVER = "/Volumes/MOE/bin/geckodriver"
 VISIT_LIMIT = 33
 TEMPLATE_ARTICLE_XPATH = "/html/body/div[1]/div[1]/div[6]/div[2]/div[3]/div/div/div/div/div/div[2]/div[%ARTICLE_ID%]"
+TEMPLATE_TOC = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+<ncx
+    xmlns="http://www.daisy.org/z3986/2005/ncx/"
+    xmlns:fo="http://www.w3.org/1999/XSL/Format"
+    xmlns:fb="http://www.gribuser.ru/xml/fictionbook/2.0"
+    xmlns:xlink="http://www.w3.org/1999/xlink" version="2005-1">
+    <head>
+        <meta name="dtb:uid" content="7884d7d2-efe1-11e3-871d-0025905a0812"/>
+        <meta name="dtb:depth" content="3"/>
+        <meta name="dtb:totalPageCount" content="0"/>
+        <meta name="dtb:maxPageNumber" content="0"/>
+    </head>
+    <navMap>
+%TOC%
+    </navMap>
+</ncx>
+"""
+TEMPLATE_TOC_NAV_POINT = """
+<navPoint id="%POINT_ID%" playOrder="%POINT_ORDER%">
+    <navLabel>
+        <text>%POINT_TITLE%</text>
+    </navLabel>
+    <content src="%POINT_SRC%"/>
+    %SUBITEMS%
+</navPoint>
+"""
 XPATH_NEXT_MONTH = "/html/body/div[1]/div[1]/div[6]/div[2]/div[3]/div/div/div/nav/div[3]"
