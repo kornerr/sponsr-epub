1f7b5f5eac946c5b8e071caf67c54d4650e569cc
Михаил Капелько <kornerr@gmail.com>
2025-07-10 23:38:54 +0300

    save content

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-from DoGenerateContent import *
+from DoExtractContent import *
 from DoGenerateTOC import *
 from DoVisitArticles import *
 from constants import *
 
 tasks = [
     #DoVisitArticles(FILE_CACHE_VISIT),
+    DoExtractContent(FILE_CACHE_CONTENT, FILE_CACHE_VISIT),
     DoGenerateTOC(FILE_CACHE_TOC, FILE_CACHE_VISIT),
-    DoGenerateContent(FILE_CACHE_VISIT),
 ]
 
 for task in tasks:
